1bc8e0f753df13cf5b5577fd44eadea70211785c
Romario-Negreiros <nromario482@gmail.com>
2025-07-18 18:51:43 -0300

    buildar

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,37 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Admin from "./pages/Admin";
 import NotFound from "./pages/NotFound";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Index />,
+    },
+    {
+      path: "/admin",
+      element: <Admin />,
+    },
+    // ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ],
+  {
+    basename: "/agendamentoparanapiacaba/",
+  }
+);
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/admin" element={<Admin />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </TooltipProvider>
   </QueryClientProvider>
 );
